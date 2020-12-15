c6baae639ba35f3d80812c126b9cf699ccc62af1
Matteo Cilli <cilli.matteo@gmail.com>
2020-12-15 18:11:48 +0100

    Strike Package Bravo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 
+
 const app = express(); // starting our express app
 app.set('view engine', 'pug'); // setting to use pug
 app.use('/static', express.static('public')); // setting the public folder at the address /static
@@ -10,11 +11,24 @@ const homeRoute = require('./routes');
 const aboutRoute = require('./routes/about');
 const projectsRoute = require('./routes/projects');
 
-
 // use routes in pair with their javascript
 app.use(homeRoute);
 app.use('/about', aboutRoute);
 app.use('/projects', projectsRoute);
+
+// create a new error message for when the user tries to enter an unknown page
+app.use((req, res, next) => {
+    const err = new Error('Page not found!');
+    err.status = 404;
+    next(err);
+});
+
+// renders the error page
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.render('error', err);
+    console.log("Oops! The page you are looking for cannot be found.");
+});
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!');
